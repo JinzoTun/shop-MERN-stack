e0faa9b96b3dacbc736b9a1da04cc8b3168249cf
Koussay Hajri <hajrikoussay03@gmail.com>
2024-02-26 23:52:22 +0100

    mongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,34 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// connect to Mongodb and console logging
+
+mongoose.connect(MONGODB_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.log('Error connecting to MongoDB:', err.message);
+    });
+
+
+
+
+
 
